c5d0b371020d973d24090c2249c79414b92e77db
Adler Coelho Santos <adler.coelho.santos@icloud.com>
2017-12-28 21:25:53 -0200

    Terminando o app de frases randômicas.

--- app2/index.android.js
+++ app2/index.android.js
@@ -1,31 +1,56 @@
 import React from 'react';
-import { Text, View, Button, AppRegistry } from 'react-native';
+import { Text, View, Button, TouchableOpacity, Image, AppRegistry, Alert } from 'react-native';
 
 const Estilos = {
-  estiloTexto: {
-    fontSize: 30,
-    fontWeight: '300',
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: '#08509B',
-    color: '#fff',
-    textAlign: 'center',
-  },
   estiloView: {
-    backgroundColor: 'skyblue',
-    height: 300,
+    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center'
+    paddingVertical: 20
+  },
+  botao: {
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 20
+  },
+  txtBotao: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 20
   }
 }
 
 const App = () => {
 
-  const { estiloTexto, estiloView } = Estilos;
+  const {  estiloView, botao, txtBotao, } = Estilos;
+
+  const gerarNovaFrase = () => {
+
+    let frases = [
+      'Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.',
+      'O ciumento passa a vida a procurar um segredo, cuja descoberta lhe destruiria a felicidade.',
+      'O progresso do homem não é mais do que uma descoberta gradual de que as suas perguntas não têm significado.',
+      'A descoberta consiste em ver o que todos viram e em pensar no que ninguém pensou.',
+      'Se um homem não descobriu nada pelo qual morreria, não está pronto para viver.'
+    ];
+
+    let random = Math.floor(Math.random() * frases.length);    
+
+    Alert.alert('Frase escolhida', frases[random]);
+  }
 
   return (
     <View style={estiloView}>
-      <Text style={estiloTexto}>Frases do dia</Text>
+      <Image 
+        source={require('./imgs/logo.png')}
+      />
+      <TouchableOpacity
+        style={botao}
+        onPress={gerarNovaFrase}
+      >
+        <Text style={txtBotao}>Nova frase</Text>
+      </TouchableOpacity>
     </View>
   );
 };
